0377142a912b415ce2b93b9fe56bd8f0835eecb7
Aditya Bhosale <aadibhosale001@gmail.com>
2026-02-13 22:59:58 +0530

    Day 3 Complete

---
+++ DAY 3/Patternprint2.java
@@ -0,0 +1,13 @@
+
+    public class Patternprint2{
+public static void main(String[] args) {
+    for(int line=1;line<=4;line++){
+        for(int j=4;j>=line;j--){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}    
+}
+
+
