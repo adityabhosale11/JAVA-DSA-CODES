f61b1c18ddf9fe259a159259b7caf76c4433dbec
Aditya Bhosale <aadibhosale001@gmail.com>
2026-02-22 18:31:23 +0530

    FOLDER RENAMED

--- DAY 4/Average.java
+++
@@ -1,9 +0,0 @@
-public class Average {
-    public static double average(double a,double b,double c){
-        double average=(a+b+c)/3;
-        return average;
-    }
-    public static void main(String[] args) {
-        System.out.println("THE AVERAGE IS..."+average(4, 6, 7));
-    }
-}
--- DAY 4/functions.java
+++
@@ -1,8 +0,0 @@
-public class functions {
-    public static void helloworld(){
-        System.out.println("hello world");
-    }
-    public static void main(String[] args) {
-        helloworld();
-    }
-}
